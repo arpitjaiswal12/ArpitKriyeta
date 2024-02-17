ee33ab87fc9b62d957630a0ed5929123aa918c7d
Arpit_Jaiswal <jaiswalarpit1414@gmail.com>
2024-02-17 11:24:31 +0530

    completed AboutUs page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Navbar from './Componets/Navbar.js';
 import Home from './pages/Home.js';
 import Addknowledgebase from './pages/Addknowledgebase.js';
 import Productcart from './Componets/Productcart.js';
+import AboutUs from './pages/AboutUs.js';
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
           <Route path='/home' element={<Home/> }/>
           <Route path='/content' element={<Addknowledgebase/> }/>
           <Route path='/product' element={<Productcart/> }/>
+          <Route path='/about' element={<AboutUs/> }/>
         </Routes>
       </Router>
       
